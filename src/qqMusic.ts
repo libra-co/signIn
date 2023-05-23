94aaeb9c9ef1f0a525dc01251c62cdbe6661d774
liuhongbo <916196375@qq.com>
2023-05-23 21:40:50 +0800

    feat: change log msg

--- src/qqMusic.ts
+++ src/qqMusic.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: liuhongbo 916196375@qq.com
+ * @Date: 2023-05-21 23:08:25
+ * @LastEditors: liuhongbo 916196375@qq.com
+ * @LastEditTime: 2023-05-23 21:39:49
+ * @FilePath: \signInPlug\src\qqMusic.ts
+ * @Description: QQ 音乐
+ */
 import axios from "axios"
 import { getNowTime, pushMsg } from ".";
 const { qqMusic } = require('./config')
@@ -10,14 +18,14 @@ export const signInQqMusic = async (backupUrl?: string) => {
         headers
     })
     if (res && res.data && res.data.msg.indexOf('失败') === -1) {
-        pushMsg('音乐加速', res.data.msg)
+        pushMsg('音乐加速', '连接1' + res.data.msg)
     } else {
         if (backupUrl) {
             pushMsg('音乐加速', '音乐加速失败')
         }
         if (res.data.msg.indexOf('失败') !== -1) {
             signInQqMusic(qqMusic.backupUrl)
-            pushMsg('音乐加速', res.data.msg)
+            pushMsg('音乐加速', '备份连接' + res.data.msg)
         }
     }
 }
